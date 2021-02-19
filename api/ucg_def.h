86910cb3ea49354eaa96bae7237690e75482b1d9
chenyidu <69713632+chenyidu@users.noreply.github.com>
2021-02-19 15:37:00 +0800

    Update ucg_def.h

--- api/ucg_def.h
+++ api/ucg_def.h
@@ -17,4 +17,36 @@ typedef enum ucg_distance {
     UCG_DISTANCE_NET,
 } ucg_distance_t;
 
+/**
+ * @ingroup UCG_CONTEXT
+ * @brief UCG Application Context
+ */
+typedef struct ucg_context* ucg_context_h;
+
+/**
+ * @ingroup UCG_CONFIG
+ * @brief UCG configuration descriptor
+ */
+typedef struct ucg_config ucg_config_t;
+
+ /**
+  * @ingroup UCG_GROUP
+  * @brief UCG Group
+  *
+  * UCG group is used for collective operations. Groups are created with respect to a local
+  * worker, and share its endpoints for communication with the remote workers.
+  */
+typedef struct ucg_group* ucg_group_h;
+
+ /**
+  * @ingroup UCG_REQUEST
+  * @brief UCG collective operation request
+  *
+  * UCG request is an opaque object representing a description of a collective
+  * operation. The description holds all the
+  * necessary information to perform collectives, so re-starting an operation
+  * requires no additional parameters.
+  */
+typedef void* ucg_request_h;
+
 #endif
