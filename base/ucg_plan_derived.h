bbb08809cf5bf8dff9f9a172fbf05f63ffad9c64
chenyidu <69713632+chenyidu@users.noreply.github.com>
2021-02-22 14:43:25 +0800

    Update and rename ucg_plan_inner.h to ucg_plan_derived.h

--- base/ucg_plan_derived.h
+++ base/ucg_plan_derived.h
@@ -3,8 +3,6 @@
  * See file LICENSE for terms.
  */
 
-/* The header file cannot be directly contained except for the derived plans. */ 
-
 #ifndef UCG_PLAN_INNER_H_
 #define UCG_PLAN_INNER_H_
 
@@ -20,10 +18,11 @@
     }
 
 /**
- * Define a static object of plan.
- * Example: UCG_PLAN_STATIC_DEFINE_AND_REGISTER(ktree, bcast, BCAST, "Using knonimal tree to broadcast.")
+ * Define a static object of plan and register to plan pool.
+ * Example: UCG_PLAN_STATIC_DEFINE_AND_REGISTER(ktree, bcast, BCAST, "Using k-nonimal tree to broadcast.")
  */
-#define UCG_PLAN_STATIC_DEFINE_AND_REGISTER(_name, _type, _uppercase_type, _description) \
+#define UCG_PLAN_STATIC_DEFINE_AND_REGISTER(_name, _type, _uppercase_type, _description, \
+                                            _clone, _is_available, _query) \
     static ucg_plan_##_type##_t ucg_plan_##_type##_##_name = {\
         .refcount = 1, /* Static object can not be released. */ \
         .type = UCG_PLAN_TYPE_##_upcase_type, \
@@ -31,9 +30,19 @@
         .description = _description, \
         .action_cnt = 0, \
         .action = NULL, \
+        .clone = _clone, \
+        .is_available = _is_available, \
+        .query = _query, \
     }; \
     UCG_PLAN_REGISTER(&ucg_plan_##_type##_##_name)
-     
+
+/**
+ * @ingroup UCG_PLAN_ACTION
+ * @brief Abstracted action type.
+ *
+ * At present, The way of SEND, RECV and REDUCE are performed is very clear. 
+ * Keep a generic type for some special actions.
+ */        
 typedef enum ucg_plan_action_type {
     UCG_PLAN_ACTION_TYPE_SEND,
     UCG_PLAN_ACTION_TYPE_RECV,
@@ -42,6 +51,10 @@ typedef enum ucg_plan_action_type {
     UCG_PLAN_ACTION_TYPE_MAX,
 } ucg_plan_action_type_t;
 
+/**
+ * @ingroup UCG_PLAN_ACTION
+ * @brief Parameters for performing action.
+ */
 typedef ucg_plan_action_params {
     struct {
         uint8_t *data; 
@@ -85,7 +98,7 @@ typedef struct ucg_plan_action_buf {
  * @ingroup UCG_PLAN_ACTION
  * @brief Infrastructure of an action.
  *
- * When cloning a plan, something in the action is no need to change, we call it 
+ * When cloning a plan, something in a action has no need to change, we call it 
  * the infrastructure of an action which can be reused directly.
  */
 typedef struct ucg_plan_action_infra {
@@ -114,6 +127,35 @@ struct ucg_plan_action {
     ucg_plan_action_t* next;
 };
 
+typedef struct ucg_plan_attr {
+    
+} ucg_plan_attr_t;
+
+/**
+ * @ingroup UCG_PLAN
+ * @brief Check whether the plan is available.
+ * 
+ * @return 1-is available, 0-not available.
+ */
+typedef int (*ucg_plan_is_available_cb_t)(ucg_plan_t *plan, ucg_plan_params_t *params);
+
+/**
+ * @ingroup UCG_PLAN
+ * @brief Get plan's attribution.
+ *
+ * This interface is used to obtain some scoring information from plan, so that 
+ * we can select the best plan from multiple plans.
+ */
+typedef ucs_status_t (*ucg_plan_query_cb_t)(ucg_plan_t *plan, ucg_plan_attr_t* attr);
+
+/**
+ * @ingroup UCG_PLAN
+ * @brief Clone a plan.
+ *
+ * This interface should return a initialized plan which has generated all actions.
+ */
+typedef ucg_plan_t* (*ucg_plan_clone_cb_t)(ucg_plan_t *plan, ucg_plan_params_t *params);
+
 /**
  * @ingroup UCG_PLAN
  * @brief Base structure of plan.
@@ -124,15 +166,17 @@ typedef struct ucg_plan {
     const char *name;
     const char *description;
  
-    itn action_cnt;
-    ucg_plan_action_t *action;
+    int action_cnt;
+    ucg_plan_action_t *action; /* The head of an aciton list. */
  
+    ucg_plan_is_available_cb_t is_available;
+    ucg_plan_query_cb_t query;
     ucg_plan_clone_cb_t clone;
 } ucg_plan_t;
 
 /**
  * @ingroup UCG_PLAN
- * @brief Structure of broadcast plan.
+ * @brief Base structure of broadcast plan.
  */
 typedef strcut ucg_plan_bcast {
     ucg_plan_t super;
@@ -141,7 +185,7 @@ typedef strcut ucg_plan_bcast {
 
 /**
  * @ingroup UCG_PLAN
- * @brief Structure of broadcast plan.
+ * @brief Base structure of allreduce plan.
  */
 typedef strcut ucg_plan_allreduce {
     ucg_plan_t super;
@@ -150,7 +194,7 @@ typedef strcut ucg_plan_allreduce {
 
 /**
  * @ingroup UCG_PLAN
- * @brief Structure of broadcast plan.
+ * @brief Base structure of barrier plan.
  */
 typedef strcut ucg_plan_barrier {
     ucg_plan_t super;
